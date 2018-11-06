a326e665b7b27d31f94f6059408a0c887923a48d
Bertrand Nancy <bnancy397@gmail.com>
2018-11-06 14:24:06 +0100

    Reorganize code

---
+++ io.js
@@ -0,0 +1,57 @@
+const db = require('./db')
+const debug = require('debug')('nodeefrei:server')
+
+const initIO = (server) => {
+  const io = require('socket.io')(server)
+  io.on('connection', function (socket) {
+    socket.on('login', (msg) => {
+      debug(`got token ${msg}`)
+      db.getUserByToken(msg, (err, row) => {
+        socket.emit('user info', row)
+      })
+    })
+    socket.on('channels-request', () => {
+      db.getChannels((err, rows) => {
+        socket.emit('channels', rows)
+      })
+    })
+    socket.on('channel-messages-request', (msg) => {
+      db.getMessagesForChannel(msg.channel, (err, rows) => {
+        if (err || !rows) {
+          console.log(err)
+          return
+        }
+        for (let message of rows) {
+          socket.emit('channel-message', {
+            channel: message.channel,
+            content: message.content,
+            user: message.user
+          })
+        }
+      })
+    })
+    socket.on('channel-message', (msg) => {
+      db.getUserByToken(msg.token, (err, user) => {
+        if (err || !user) {
+          return
+        }
+        db.getChannel(msg.channel, (err, row) => {
+          if (err || !row) {
+            debug('Can\'t create mesage')
+            return
+          }
+          debug(`Creating new message ${msg.content} for user ${user.username} in channel ${row.channel}`)
+          db.createMessage(user.username, row.channel, msg.content)
+          io.emit('channel-message', {
+            channel: row.channel,
+            content: msg.content,
+            user: user.username,
+            admin: row.admin
+          })
+        })
+      })
+    })
+  })
+}
+
+module.exports = initIO
